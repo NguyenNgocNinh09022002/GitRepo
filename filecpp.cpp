adc342d8ee9aea119dd3ba9633849d34134d2898
NguyenNgocNinh <ninhnguyenchibi@gmail.com>
2023-02-25 20:19:35 +0700

    commit moi luc 8h19 ngay 25 thang 2

--- filecpp.cpp
+++ filecpp.cpp
@@ -3,8 +3,11 @@ using namespace std;
 
 int main()
 {
+	cout << "in moi luc 8h 168 phut ngay 25 thang 2";
     print("hello world");
     print("command to test pull");
     print("this code is created in local repo");
-    print("this code to pull request") return 0;
+    print("this code to pull request");
+    cout << "chac ok roi nhi";
+    return 0;
 }
